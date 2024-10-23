5c5084cb0e557f8522e0e78466fffba1c19fe71c
trinhhandsome <trinhtrong0505@gmail.com>
2024-10-23 18:49:32 +0700

    change

--- FormBeam.Designer.cs
+++ FormBeam.Designer.cs
@@ -28,40 +28,24 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.metroListView1 = new MetroFramework.Controls.MetroListView();
-			this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.Mmax = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.Nmax = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.Qmax = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.pn_Menu = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
-			// metroListView1
+			// pn_Menu
 			// 
-			this.metroListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.ID,
-            this.Name,
-            this.Size,
-            this.Mmax,
-            this.Nmax,
-            this.Qmax});
-			this.metroListView1.Font = new System.Drawing.Font("Segoe UI", 12F);
-			this.metroListView1.FullRowSelect = true;
-			this.metroListView1.Location = new System.Drawing.Point(179, 57);
-			this.metroListView1.Name = "metroListView1";
-			this.metroListView1.OwnerDraw = true;
-			this.metroListView1.Size = new System.Drawing.Size(540, 209);
-			this.metroListView1.TabIndex = 0;
-			this.metroListView1.UseCompatibleStateImageBehavior = false;
-			this.metroListView1.UseSelectable = true;
+			this.pn_Menu.Dock = System.Windows.Forms.DockStyle.Left;
+			this.pn_Menu.Location = new System.Drawing.Point(0, 0);
+			this.pn_Menu.Name = "pn_Menu";
+			this.pn_Menu.Size = new System.Drawing.Size(200, 450);
+			this.pn_Menu.TabIndex = 0;
 			// 
 			// frm_Beam
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.metroListView1);
+			this.Controls.Add(this.pn_Menu);
+			this.Name = "frm_Beam";
 			this.Text = "FormBeam";
 			this.ResumeLayout(false);
 
@@ -69,12 +53,6 @@
 
 		#endregion
 
-		private MetroFramework.Controls.MetroListView metroListView1;
-		private System.Windows.Forms.ColumnHeader ID;
-		private System.Windows.Forms.ColumnHeader Name;
-		private System.Windows.Forms.ColumnHeader Size;
-		private System.Windows.Forms.ColumnHeader Mmax;
-		private System.Windows.Forms.ColumnHeader Nmax;
-		private System.Windows.Forms.ColumnHeader Qmax;
+		private System.Windows.Forms.Panel pn_Menu;
 	}
 }
